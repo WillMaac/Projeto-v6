e08fcb500600fa736371cd2b0c65cc48f2c2cb9c
WillMaac <anderson.wjj@hotmail.com>
2025-03-21 16:21:11 -0300

    Página e component not found

--- Projeto/src/main.tsx
+++ Projeto/src/main.tsx
@@ -9,6 +9,7 @@ import { About } from "./components/About/About";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Post } from "./components/Post/Post";
 import { Redirect } from "./components/Redirect/Redirect";
+import { NotFound } from "./components/NotFound/NotFound";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
         <Route path="/redirect" element={<Redirect />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
