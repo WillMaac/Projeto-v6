23e3b30fd6906249ebcf5a545dc001da022642f1
WillMaac <anderson.wjj@hotmail.com>
2025-03-21 16:52:41 -0300

    Finalizado

--- Projeto/src/components/About/About.tsx
+++ Projeto/src/components/About/About.tsx
@@ -1,10 +1,15 @@
+import { useLocation } from "react-router-dom";
 import "./styles.css";
 
 
 export const About = () => {
+  const {state} = useLocation();
+
+
   return (
     <div>
       <h1>Página About</h1>
+      <p>{state as string}</p>
     </div>
   );
 };
